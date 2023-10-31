dedb96d035fcfafb5ebec9846fc6e9e4a18cf84a
Daniel Morley <morleydan10@gmail.com>
2023-10-31 05:13:45 -0400

    first-class-functions

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction (callback){
+    return callback ();
+};
+
+function returnsANamedFunction(){
+    return function namedFunction() {
+        console.log("This is a named function");
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function () {
+        console.log("This is an anonymous function");
+    }
+}
